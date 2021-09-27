5adae0c2a99961e92b3f71b31cbc046e75c09cba
EC2 Default User <ec2-user@ip-172-31-79-216.ec2.internal>
2021-09-27 09:03:13 +0000

    trial code 1

--- ass3app/src/main/java/au/edu/scu/app/App.java
+++ ass3app/src/main/java/au/edu/scu/app/App.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.Map;
-
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
@@ -26,39 +25,45 @@ public class App implements RequestHandler<Map<String, Object>, String>
     @Override
     public String handleRequest(Map<String, Object> input, Context context) {
         
-       int id = (int) input.get("id");
+      int id = (String) input.get("id");
        String message = "";
        
        
-       //retrieve Car table entry
-       AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build(); 
-       DynamoDB ddb = new DynamoDB(client);
+      //retrieve Car table entry
+      AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build(); 
+      DynamoDB ddb = new DynamoDB(client);
        
-       try {
-           Table table = ddb.getTable("Car");
-           
-           GetItemSpec spec = new GetItemSpec()
-        .withPrimaryKey("Id", id)
-        .withProjectionExpression("Id, Make, Model, Year, Colour")
-        .withConsistentRead(true);
-        
-        Item item = table.getItem(spec);
-        System.out.println(item.toJSONPretty());
+      try {
+          result = ddb.getTable("Car").getItem("Id", id);
+          
+          if(result = null) {
+           message = "ID does not exist. Try Again.";
+           return message;
+            
+              
+          }
+          else {
+
+              
+            int id = item.getInt(String "Id");
+            String make = item.getJSON(String "Make");
+            String model = item.getJSON(String "Model");
+            int year = item.getString("Year");
+            String colour = item.getString("Colour");
+            
+            message = "Id: " + id + ", Make: " + make + ", Model: " + model + ", Year: " + year + ", Colour: " + color;
+            return message;
+
+          }
 
            
-           //add if-condition for null object = error message
-           //if found get id and attributes assigned
            
-        //   String make = item.getString("Make");
-        //   int year = item.getInt("Year");
-        //   message = "Id: " + id + ", Make: " + make + ", Model: " + model + ", Year: " + year + ", Colour: " + color;
-        //   return message;
-       }
-       catch(Exception exp) {
+
+      }
+      catch(Exception exp) {
            
-       }
+      }
        
-       return message;
         
     }
 }
